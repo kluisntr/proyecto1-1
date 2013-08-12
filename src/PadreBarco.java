4348e502139b4e74182bee3559273f51823e7068
Guillermo2013 <memojose_02@hotmail.com>
2013-08-11 21:41:48 -0600

    prueba

---
+++ src/PadreBarco.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Guillermo Sandoval
+ */
+public class PadreBarco {
+  protected int posicion;
+
+    public PadreBarco(int x){
+        posicion =x;
+}
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+
+
+}
